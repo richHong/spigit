b59e4d3546912f0e58b5b68515019d19a6c3f6cf
RichHong <rich.hong@gmail.com>
2016-10-13 18:04:23 -0700

    data comes in from back to front end

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function($scope){
-  $scope.data = {text: 'Hello World!'};
+app.controller('listController', function($scope, dataFactory){
+  $scope.data = {text: 'Hello World'};
+  dataFactory.getData().then(function(data){
+    console.log(data);
+    $scope.data.activities = data.recentActivities;
+    console.log($scope.data)
+  });
+});
+
+app.factory('dataFactory', function($http){
+
+  var getData = function(){
+    return $http({
+      method: 'GET',
+      url: '/api/data'
+    }).then(function(response){
+      return response.data.data;
+    });
+  };
+
+  return {
+    getData: getData
+  };
 });
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var data = require('./data.json');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
+app.get('/api/data', function(req,res){
+
+  res.send(data);
+});
 
 app.listen(8080, function(){
   console.log('listening on localhost:8080');
